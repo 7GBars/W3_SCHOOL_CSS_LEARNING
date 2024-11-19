fdb50afee05e792a374a53d6df06d42fc3ad3f10
George <7GBars@gmail.com>
2024-11-19 23:39:23 +0300

    table: -реализовать автоматические простановку колонок и столбцов в таблице

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Table} from "./components/Table";
+import {customers} from "./__mocks__/customers";
 
 function App() {
   return (
     <div className="App">
-      <Table width={'100%'} rowHeight={40}/>
+      <Table width={'100%'} rowHeight={40} data={customers} columns={['CompanyName', 'City', 'Phone']} />
 
     </div>
   );
--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,10 +1,12 @@
-import React, { FC } from 'react';
+import React, {FC, memo, useMemo} from 'react';
 import styled from 'styled-components';
 import './index.scss';
 
 type TTableProps = {
   width: number | string;
   rowHeight: number;
+  data: any[];
+  columns: string[];
 }
 
 // Создаем стилизованный компонент для строк таблицы за пределами функционального компонента
@@ -12,33 +14,32 @@ const TableRow = styled.tr<{ rowHeight: number }>`
   height: ${props => props.rowHeight}px;
 `;
 
-export const Table: FC<TTableProps> = ({ rowHeight, width }) => {
-  return (
-    <table className={'my_table'} style={{ width }}>
-      <thead>
-      <TableRow rowHeight={rowHeight}>
-        <th>Name</th>
-        <th>ManOrWomen</th>
-        <th>Age</th>
+export const Table: FC<TTableProps> = memo(({rowHeight, width, data, columns}) => {
+  const memoizedColumns = useMemo(() => {
+    return columns.map(c => <th>{c}</th>)
+  }, [columns]);
+
+  const memoizedRows = useMemo(() => {
+    return data.map(rowData => {
+      return <TableRow rowHeight={rowHeight}>
+        {columns.map(c => {
+          console.log('rowData, ', rowData)
+          return <td>{rowData[c]}</td>
+        })}
       </TableRow>
+    })
+  }, data)
+
+  return (
+    <table className={'my_table'} style={{width}}>
+      <thead>
+      <tr>
+        {memoizedColumns}
+      </tr>
       </thead>
       <tbody>
-      <TableRow rowHeight={rowHeight}>
-        <td>Alfreds Futterkiste</td>
-        <td>man</td>
-        <td>20</td>
-      </TableRow>
-      <TableRow rowHeight={rowHeight}>
-        <td>Nancy</td>
-        <td>women</td>
-        <td>25</td>
-      </TableRow>
-      <TableRow rowHeight={rowHeight}>
-        <td>Ash</td>
-        <td>man</td>
-        <td>25</td>
-      </TableRow>
+      {memoizedRows}
       </tbody>
     </table>
   );
-}
+})
