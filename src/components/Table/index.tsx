cb23e0c8c79c13ca17ce44fc6c57c1d2d8e1622f
George <7GBars@gmail.com>
2024-11-29 00:30:53 +0300

    table: -реализовать смену поз ций колонок через dragAndDrop

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,4 +1,4 @@
-import React, {FC, memo, useMemo} from 'react';
+import React, {FC, memo, useCallback, useMemo, useRef, useState} from 'react';
 import styled from 'styled-components';
 import './index.scss';
 
@@ -15,6 +15,12 @@ type TTableProps = {
   width: number | string;
 }
 
+enum TAG_NAME {
+  TD = 'TD',
+  TH = 'TH',
+  OTHER = 'OTHER'
+}
+
 // Создаем стилизованный компонент для строк таблицы за пределами функционального компонента
 const TableRow = styled.tr<{rowConfig: TRowConfig}>`
   height: ${({rowConfig: {height}}) => height}px;
@@ -28,24 +34,95 @@ const TableRow = styled.tr<{rowConfig: TRowConfig}>`
 `;
 
 export const Table: FC<TTableProps> = memo(({rowConfig, width, data, columns}) => {
+
+  const [columnOrder, setColumnOrder] = useState(columns);
+  const [draggedColumn, setDraggedColumn] = useState<string | null>(null);
+  const isInsideDrop = useRef<boolean>(false);
+
+  const tableRef = useRef<HTMLDivElement>(null);
+
+
+
+  const handleDragLeave = useCallback((e: React.DragEvent) => {
+    e.stopPropagation();
+
+    const relatedElement = e.relatedTarget as HTMLElement;
+
+    if(relatedElement === null) {return}
+    console.log(relatedElement)
+    if (relatedElement.tagName === TAG_NAME.TD) {
+      isInsideDrop.current = false;
+    } else if (relatedElement.tagName === TAG_NAME.TH) {
+      isInsideDrop.current = true;
+    } else {
+      console.log('e.relatedTarget ', e.relatedTarget )
+    }
+    // Проверяем, действительно ли курсор покинул контейнер таблицы
+    // if (tableRef.current?.contains(e.relatedTarget as Node)) {
+    //   console.log('Drag left the table container');
+    //   isInsideDrop.current = false;
+    // } todo @bar - почему не работает
+  }, []);
+
+  const handleDragStart = useCallback((column: string) => {
+    console.log('Drag started');
+    setDraggedColumn(column);
+  }, []);
+
+  const handleDragOver = useCallback((e: React.DragEvent) => {
+    e.preventDefault(); // Чтобы разрешить drop
+  }, []);
+
+  const handleDrop = useCallback(
+    (targetColumn: string) => {
+      if (!draggedColumn) return;
+      const newOrder = [...columnOrder];
+      const fromIndex = newOrder.indexOf(draggedColumn);
+      const toIndex = newOrder.indexOf(targetColumn);
+
+      newOrder.splice(fromIndex, 1);
+      newOrder.splice(toIndex, 0, draggedColumn);
+
+      setColumnOrder(newOrder);
+      console.log('Dropped on column:', targetColumn);
+    },
+    [draggedColumn, columnOrder]
+  );
+
   const memoizedColumns = useMemo(() => {
-    return columns.map(c => <th>{c}</th>)
-  }, [columns]);
+    return columnOrder.map(col => <th
+      key={col}
+      draggable
+      onDragStart={() => handleDragStart(col)}
+      onDragOver={handleDragOver}
+      onDrop={() => handleDrop(col)}
+      className="draggable-header"
+    >
+      {col}
+    </th>)
+  }, [columnOrder]);
 
   const memoizedRows = useMemo(() => {
-    return data.map(rowData => {
-      return <TableRow rowConfig={rowConfig}>
-        {columns.map(c => {
+    return data.map((rowData, index) => {
+      return <TableRow rowConfig={rowConfig} key={index}>
+        {columnOrder.map(c => {
           console.log('rowData, ', rowData)
-          return <td>{rowData[c]}</td>
+          return <td key={columnOrder.indexOf(c)}>{rowData[c]}</td>
         })}
       </TableRow>
     })
-  }, data)
+  }, [data, columnOrder])
+
+
+
 
   return ( <div className={'table-component'}>
 
-      <div className={'table-container'}>
+      <div
+        className={'table-container'}
+        onDragLeave={handleDragLeave}
+        ref={tableRef}
+      >
         <table className={'table-container__table'} style={{width}}>
           <thead>
           <tr>
