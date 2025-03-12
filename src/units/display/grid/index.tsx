170f344799db7d70b88e34b64688a7bb6cc70fcb
George <7GBars@gmail.com>
2025-03-12 22:18:36 +0300

    units/grid: -добавить грид-контейнер

---
+++ src/units/display/grid/index.tsx
@@ -0,0 +1,17 @@
+import React, {FC} from 'react';
+
+import './index.scss';
+
+type TGridContainerProps = {};
+
+export const GridContainer: FC<TGridContainerProps> = ({}) => {
+  return (
+    <div className={'grid-container'}>
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>4</div>
+      <div>5</div>
+    </div>
+  );
+}
--- src/units/display/index.tsx
+++ src/units/display/index.tsx
@@ -2,11 +2,14 @@ import React, {PropsWithChildren} from 'react';
 
 import './index.scss';
 
+import {GridContainer} from "./grid";
+
 
 type TDisplayProps = {}
 
 export const ContentsContainer = ({children}: PropsWithChildren) => {
   return <div className={'container--contents'}>
-    {children}
+    {children} {/* todo @bars - понять как реализовать дополнительные подстраницы */}
+    <GridContainer />
   </div>
 }
