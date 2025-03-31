b91c1ba4a0b0504291f137fe0b1fb52cd067abdd
George <7GBars@gmail.com>
2025-04-01 00:11:00 +0300

    GridPage: -разделить блок кнопок и блок текущего примера

--- src/pages/display/grid/index.tsx
+++ src/pages/display/grid/index.tsx
@@ -3,6 +3,7 @@ import React, { FC, useMemo, useState } from 'react'
 import { Grid } from "@/units";
 import { GButton } from "@/components";
 
+import './index.scss';
 
 type TGridPageProps = {};
 
@@ -17,21 +18,30 @@ type TUnitsNames = typeof units[number]['name'];
 export const GridPage: FC<TGridPageProps> = ({}) => {
   const [currentExample, setCurrentExample] = useState<TUnitsNames>(units[0].name);
 
-  const gridExamples = useMemo(() => (
+  const exampleButtons = useMemo(() => (
     units.map(example => (
       <React.Fragment key={example.name}>
         <GButton
           textOption={{ caption: example.name, isVisible: true }}
           onClick={() => setCurrentExample(example.name)}
         />
-        {currentExample === example.name && example.component && <example.component />}
       </React.Fragment>
     ))
   ), [currentExample]);
 
+ const currentExampleComponent = useMemo(() => {
+   const currentExampleComponent = units.find(unit => unit.name === currentExample);
+   return currentExampleComponent?.component && <currentExampleComponent.component />;
+ }, [currentExample]);
+
   return (
     <div className={'grid-page-container'}>
-      {gridExamples}
+      <div className={'grid-page-container__buttons'}>
+        {exampleButtons}
+      </div>
+      <div className={'grid-page-container__example-block'}>
+        {currentExampleComponent}
+      </div>
     </div>
   );
 }
