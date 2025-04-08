74d08c1d389365bfb30145c752e02513b6733f18
Barsegyan <barsegyan@topsystems.ru>
2025-04-08 19:53:07 +0300

    pages/units: -реализовать базовую версию с text-overflow

--- src/units/display/flex/simple/index.tsx
+++ src/units/display/flex/simple/index.tsx
@@ -1,17 +1,24 @@
 import React, {FC} from 'react';
 
 import './index.scss';
+import { GButton } from "@/components";
 
 type TFlexContainerProps = {};
 
 export const FlexContainer: FC<TFlexContainerProps> = ({}) => {
   return (
     <div className={'flex-container'}>
-      <div>1</div>
-      <div>2</div>
-      <div>3</div>
-      <div>4</div>
-      <div>5</div>
+      <div className="flex_container__element command-bar">
+        <div className={'command-bar_item'}>
+          <GButton className={'button_overflow'} textOption={{caption: 'Test data Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque explicabo laborum nisi quam qui', isVisible: true}}/>
+        </div>
+        <div className={'command-bar_item'}>Test Test data Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque explicabo labor </div>
+        <div className={'command-bar_item'}>Test data</div>
+      </div>
+      <div className="flex_container__element tool-bar">
+
+        <div>Test</div>
+      </div>
     </div>
   );
 };
