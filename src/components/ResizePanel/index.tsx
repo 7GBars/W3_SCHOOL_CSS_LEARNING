003af9a34b3ebec9ccdea8dbfb37c2d50eaa4c41
George <7GBars@gmail.com>
2025-04-28 14:41:48 +0300

    resizePanel: -реализовать простую анимацию через переходы

--- src/components/ResizePanel/index.tsx
+++ src/components/ResizePanel/index.tsx
@@ -3,23 +3,26 @@ import React, {
   useState,
   useRef,
   forwardRef,
-  useImperativeHandle
+  useImperativeHandle, useCallback
 } from 'react';
 
 
 import {
-  ReflexContainer,
+  ReflexContainer, ReflexElement,
   ReflexSplitter,
 } from 'react-reflex'
 import 'react-reflex/styles.css';
 
+import './index.scss'
+
 
 import type {PaneConfig} from "./types";
-import {ControlledInnerElement} from "./ControledElement";
+
 
 
 export const ResizePanelDemo1: React.FC = () => {
   const orientation = 'vertical';
+  const [flex, setFlex] = useState(0.5);
   const [panes, setPanes] = useState<Record<string, PaneConfig>>({
     pane1: {
       id: 'pane1',
@@ -35,15 +38,31 @@ export const ResizePanelDemo1: React.FC = () => {
     }
   })
 
+  const onFullScreenHandler1 = () => {
+    const newSize = flex === 1 ? 0.5 : 1;
+    setFlex(newSize);
+  }
 
+  const onFullScreenHandler2 = () => {
+    const newSize = flex === 0 ? 0.5 : 0;
+    setFlex(newSize);
+  }
   return (
     <ReflexContainer
       orientation={orientation}
       style={{ height: '100vh' }}
+      className={'pane-content'}
     >
-      <ControlledInnerElement {...panes.pane1}/>
+      <ReflexElement flex={flex} className={'resize-panel__reflex-element'}>
+        <button onClick={onFullScreenHandler1}> fullSize </button>
+      </ReflexElement>
+
+
       <ReflexSplitter />
-      <ControlledInnerElement {...panes.pane2}/>
+      <ReflexElement className={'resize-panel__reflex-element'}>
+        <button onClick={onFullScreenHandler2}> fullSize </button>
+      </ReflexElement>
+
 
     </ReflexContainer>
   )
