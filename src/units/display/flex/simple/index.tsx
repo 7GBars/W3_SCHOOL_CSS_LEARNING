cec0145545441b3e9c4931334df23df9e0bbcf67
George <7GBars@gmail.com>
2025-05-07 19:28:16 +0300

    FlexExample: -добавить ресайз панель, -задать box-sizing border-box для всех

--- src/units/display/flex/simple/index.tsx
+++ src/units/display/flex/simple/index.tsx
@@ -1,7 +1,15 @@
 import React, { FC, useCallback, useState } from 'react';
 import styled from 'styled-components';
 
-import { classNames } from "@/helpers";
+import {
+  ReflexContainer,
+  ReflexSplitter,
+  ReflexElement
+} from 'react-reflex'
+
+import 'react-reflex/styles.css'
+
+
 import { GButton, GSelectBox } from "@/components";
 
 import './index.scss';
@@ -51,41 +59,50 @@ export const FlexContainer: FC<TFlexContainerProps> = ({}) => {
 
   return (
     <div className={'flex-example-simple'}>
-      <StyledFlexContainer
-        className={`flex-container`}
-        direction={flexDirection}
-        gap={gap}
-        wrap={flexWrapMode}
-      >
-        <StyledFlexElement className="flex-container_item" width={elementWidth}> Lorem </StyledFlexElement>
-        <StyledFlexElement className="flex-container_item" width={elementWidth}> Lorem ipsum StyledFlexElement. </StyledFlexElement>
-        <StyledFlexElement className="flex-container_item" width={elementWidth}> Lorem ipsum dolor </StyledFlexElement>
-        <StyledFlexElement className="flex-container_item" width={elementWidth}> Lorem ipsum dolor sit </StyledFlexElement>
-        <StyledFlexElement className="flex-container_item" width={elementWidth}> Lorem ipsum dolor sit amet, consectetur adipisicing. </StyledFlexElement>
-      </StyledFlexContainer>
+      <ReflexContainer orientation={'vertical'} className={'resize-panel'}>
+        <ReflexElement>
+          <StyledFlexContainer
+            className={`flex-container`}
+            direction={flexDirection}
+            gap={gap}
+            wrap={flexWrapMode}
+          >
+            <StyledFlexElement className="flex-container_item" width={elementWidth}> Lorem </StyledFlexElement>
+            <StyledFlexElement className="flex-container_item" width={elementWidth}> Lorem ipsum StyledFlexElement. </StyledFlexElement>
+            <StyledFlexElement className="flex-container_item" width={elementWidth}> Lorem ipsum dolor </StyledFlexElement>
+            <StyledFlexElement className="flex-container_item" width={elementWidth}> Lorem ipsum dolor sit </StyledFlexElement>
+            <StyledFlexElement className="flex-container_item" width={elementWidth}> Lorem ipsum dolor sit amet, consectetur adipisicing. </StyledFlexElement>
+          </StyledFlexContainer>
+        </ReflexElement>
+        <ReflexSplitter/>
+        <ReflexElement className={'commands-bar'}>
+          <GButton textOption={{caption: 'Change direction', isVisible: true}} onClick={changeDirectionHandler}/>
+          <GSelectBox
+            textOption={{caption: 'Change flex-wrap mode'}}
+            labelExpr={'mode'}
+            keyExpr={'id'}
+            options={[{mode: 'wrap', id: 1}, {mode: 'nowrap', id: 2}]}
+            onValueChange={changeWrapMode}
+          />
+          <GSelectBox<{ size: number, id: number }>
+            textOption={{caption: 'Change flex-wrap mode'}}
+            labelExpr={'size'}
+            keyExpr={'id'}
+            options={[{size: 50, id: 1}, {size: 100, id: 2}, {size: 350, id: 3}]}
+            onValueChange={changeElementsWidth}
+          />
+          <GSelectBox<{ size: [number, number], id: number, title: string }>
+            textOption={{caption: 'Change flex-wrap mode'}}
+            labelExpr={'size'}
+            keyExpr={'id'}
+            options={[{size: [20, 0], id: 1, title: 'Only row gap'}, {size: [0, 20], id: 2, title: 'Only column gap'}, {size: [50, 50], id: 3, title: 'Gap 50px'}]}
+            onValueChange={changeContainerGapSize}
+          />
+        </ReflexElement>
+      </ReflexContainer>
+
       <div className={'commands-bar'}>
-        <GButton textOption={{caption: 'Change direction', isVisible: true}} onClick={changeDirectionHandler}/>
-        <GSelectBox
-          textOption={{caption: 'Change flex-wrap mode'}}
-          labelExpr={'mode'}
-          keyExpr={'id'}
-          options={[{mode: 'wrap', id: 1}, {mode: 'nowrap', id: 2}]}
-          onValueChange={changeWrapMode}
-        />
-        <GSelectBox<{ size: number, id: number }>
-          textOption={{caption: 'Change flex-wrap mode'}}
-          labelExpr={'size'}
-          keyExpr={'id'}
-          options={[{size: 50, id: 1}, {size: 100, id: 2}, {size: 350, id: 3}]}
-          onValueChange={changeElementsWidth}
-        />
-        <GSelectBox<{ size: [number, number], id: number, title: string }>
-          textOption={{caption: 'Change flex-wrap mode'}}
-          labelExpr={'size'}
-          keyExpr={'id'}
-          options={[{size: [20, 0], id: 1, title: 'Only row gap'}, {size: [0, 20], id: 2, title: 'Only column gap'}, {size: [50, 50], id: 3, title: 'Gap 50px'}]}
-          onValueChange={changeContainerGapSize}
-        />
+
       </div>
     </div>
   );
