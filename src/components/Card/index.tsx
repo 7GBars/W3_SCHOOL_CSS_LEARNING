ad9e1c6bdcacdb499dfe36d286eb19c6ea0915e3
George <7GBars@gmail.com>
2025-05-22 16:10:23 +0300

    user-select: -убрать возможность выделять текст у лейблов selectBox, -добавить автоматическое копирование в буфер выделенного текста

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -9,8 +9,19 @@ export const Card: React.FC<TCardProps> = ({ id, title, description }) => {
   return (
     <>
       <div id={id} className={'card'}>
-        <b className={'title'}>{title}</b>
-        <p className={'description'}> {description} </p>
+        <b className={'card__title'}>{title}</b>
+        <p
+          className={'card__description'}
+          onMouseUp={() => {
+            const selection = window.getSelection();
+            const selectedText = selection?.toString().trim();
+            if (selectedText) {
+              navigator.clipboard.writeText(selectedText)
+                .then(() => alert(`Скопировано: ${selectedText}`))
+                .catch(() => alert("Не удалось скопировать текст"));
+            }
+          }}
+        > {description} </p>
       </div>
     </>
   );
